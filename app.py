ed06c19375cffedf4d5f7d4d941bd6e7d14df89f
zaycevnet <zaycebnet5@mail.ru>
2024-09-11 18:23:31 +0700

    Три кавычки

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def satrt():
-    return "<!doctype html>"\
-        "<html>"\
-            "<body>"\
-                "<h1>web-сервер на flask</h1>"\
-            "</body>"\
-        "</html>"
+    return """<!doctype html>
+        <html>
+            <body>
+                <h1>web-сервер на flask</h1>
+            </body>
+        </html>"""
 
