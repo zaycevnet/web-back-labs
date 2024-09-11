6ed8bd125033295b0be0b3bf0e823d21431579ab
zaycevnet <zaycebnet5@mail.ru>
2024-09-11 18:17:35 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -2,4 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def satrt():
-    return "web-сервер на flask"
+    return "<!doctype html>"\
+        "<html>"\
+            "<body>"\
+                "<h1>web-сервер на flask</h1>"\
+            "</body>"\
+        "</html>"
+
