234ccbb57ba1ea5fcc4345d8a5599428ac880b50
zaycevnet <zaycebnet5@mail.ru>
2024-09-12 09:25:02 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
-def satrt():
+@app.route("/web")
+def web():
     return """<!doctype html>
         <html>
             <body>
                 <h1>web-сервер на flask</h1>
+                <a href="/author">/author</a>
             </body>
         </html>"""
 
+@app.route("/author")
+def author():
+    name = "010595140"
+    group = "ФБИ-21"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href="/web">web</a>
+            </body>
+        </html>"""
