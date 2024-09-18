ab0ff25b1ad8e3a6a8cf3399b43be9a4b3437b8e
zaycevnet <zaycebnet5@mail.ru>
2024-09-18 09:31:43 +0700

    Код ответа 201

--- app.py
+++ app.py
@@ -60,3 +60,15 @@ def counter():
 @app.route("/info")
 def info():
     return redirect ("/author")
+
+@app.route("/lab1/created")
+def created():
+    return '''
+<!doctype html>
+<html>
+    <body>
+    <h1>Создано успешно</h1>
+    <div><i>что-то создано...</i></div>
+    </body>
+</html>
+''', 201
