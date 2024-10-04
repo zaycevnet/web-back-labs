900f71bd1bb835e74022af2d1766b7e6a9925b13
zaycevnet <zaycebnet5@mail.ru>
2024-10-04 19:16:28 +0700

    Вывод цветка

--- app.py
+++ app.py
@@ -271,3 +271,13 @@ def a():
 @app.route("/lab2/a")
 def a2():
     return 'без слеша'
+
+flower_list = ('роза', 'тюльпан', 'незабудка', 'ромашка')
+
+@app.route("/lab2/flowers/<int:flower_id>")
+def flowers(flower_id):
+    # return "id=" + str(flower_id)
+    if flower_id >= len(flower_list): #длина кортежа в данный момент равна 3, нумерация с 0
+        return "такого цветка нет", 404
+    else:
+        return "цветок: " + flower_list[flower_id]
