ee7fef8b900e2e14f8fb471a2de1959fc0e5c74e
zaycevnet <zaycebnet5@mail.ru>
2024-10-20 19:10:03 +0700

    Корректировка пола

--- lab3.py
+++ lab3.py
@@ -26,3 +26,10 @@ def del_cookie():
     resp.set_cookie('age')
     resp.set_cookie('name_color')
     return resp
+
+@lab3.route('/lab3/form1')
+def form1():
+    user = request.args.get('user')
+    age = request.args.get('age')
+    sex = request.args.get('sex')
+    return render_template('lab3/form1.html', user=user, age=age, sex=sex)
