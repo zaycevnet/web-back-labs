8e412765017ee625ec216d7db042256d06b35f00
zaycevnet <zaycebnet5@mail.ru>
2024-10-20 18:36:17 +0700

    Cookie

--- lab3.py
+++ lab3.py
@@ -1,6 +1,28 @@
-from flask import Blueprint, url_for, redirect, render_template
+from flask import Blueprint, render_template, request,  make_response, redirect
 lab3 = Blueprint('lab3', __name__)
+
 
 @lab3.route('/lab3/')
 def lab():
-    return render_template('lab3/lab3.html')
+    name = request.cookies.get('name')
+    name_color = request.cookies.get ('name_color')
+    return render_template('lab3/lab3.html', name=name, name_color=name_color)
+
+
+@lab3.route('/lab3/cookie')
+def cookie():
+    # return 'установка cookie', 200, {'Set-Cookie': 'name=Alex'}
+    resp = make_response(redirect ('/lab3/'))
+    resp.set_cookie('name', 'Alex', max_age=5)
+    resp.set_cookie('age', '20')
+    resp.set_cookie('name_color', 'magenta')
+    return resp
+
+
+@lab3.route('/lab3/del_cookie')
+def del_cookie():
+    resp = make_response(redirect ('/lab3/'))
+    resp.set_cookie('name')
+    resp.set_cookie('age')
+    resp.set_cookie('name_color')
+    return resp
