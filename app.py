c74035be5df1758a9abe998774aa34981faee3b0
zaycevnet <zaycebnet5@mail.ru>
2024-11-05 14:16:36 +0700

    сессия

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, redirect,  render_template #Эта функция как раз и отвечает за рендеринг шаблонов (создание html-текста для браузера):
+from flask import Flask, url_for, session, redirect,  render_template #Эта функция как раз и отвечает за рендеринг шаблонов (создание html-текста для браузера):
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
@@ -8,6 +8,7 @@ app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
+app.secret_key = 'пароль'
 #генерирует URL-адреса для маршрутов, для перенаправления 
 
 
--- lab4.py
+++ lab4.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, request
+from flask import Blueprint, render_template, redirect, request, session
 lab4 = Blueprint('lab4', __name__)
 
 @lab4.route('/lab4/')
@@ -139,14 +139,26 @@ users = [
 @lab4.route('/lab4/login',methods =['GET','POST'])
 def login():
     if request.method == 'GET':
-        return render_template ('lab4/login.html', authorized= False)
+        if 'login' in session:
+            authorized = True
+            login = session['login']
+        else:
+            authorized = False
+            login = ''
+        return render_template('lab4/login.html', authorized=authorized, login=login)
 
     login = request.form.get('login')
     password = request.form.get('password')
 
     for user in users:
         if login == user['login'] and password == user['password']:
-            return render_template ('lab4/login.html', login=login, authorized= True)
+            session['login'] = login
+            return redirect('/lab4/login')
 
     error = 'Неверный логин и/или пароль'
-    return render_template ('lab4/login.html', error=error, authorized = False)
+    return render_template('lab4/login.html', error=error, authorized=False)
+
+@lab4.route('/lab4/logout', methods=['POST'])
+def logout():
+    session.pop('login', None)
+    return redirect('/lab4/login')
