7edda871b125fcd1cccc9fbce8a6b4cbf432a80a
zaycevnet <zaycebnet5@mail.ru>
2024-11-19 09:44:36 +0700

    Регистрация

--- lab5.py
+++ lab5.py
@@ -1,7 +1,8 @@
-from flask import Flask, Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, request
+import psycopg2
 lab5 = Blueprint('lab5', __name__)
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 @lab5.route('/lab5/')
 def lab():
@@ -11,9 +12,33 @@ def lab():
 # def login():
 #     return render_template('login.html')
 
-# @lab5.route('/register')
-# def register():
-#     return render_template('register.html')
+@lab5.route('/lab5/register', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab5/register.html')
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if not (login and password):
+        return render_template ('lab5/register.html', error = 'Заполни пж все поля')
+
+    conn = psycopg2.connect(
+    host='127.0.0.1',
+    database='ser_knowledge_base',
+    user='ser_knowledge_base',
+    password='123'
+    )
+    cur = conn.cursor()
+    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
+    if cur.fetchone():
+        cur.close()
+        conn.close()
+        return render_template('lab5/register.html', error ='Такой пользователь уже существует')
+    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password}');")
+    conn.commit()
+    cur.close()
+    conn.close()
+    return render_template('lab5/success.html', login=login)
 
 # @lab5.route('/list')
 # def list_articles():
