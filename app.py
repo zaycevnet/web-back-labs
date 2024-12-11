f4aca97da19bfaf0319b142a06d6cb69d4bfc4f5
zaycevnet <zaycebnet5@mail.ru>
2024-12-11 12:15:25 +0700

    какие-то правки

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
@@ -13,6 +14,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
 # app.secret_key = 'пароль'
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет') #Устанавливает секретный ключ приложения, используемый для безопасности
@@ -72,6 +74,7 @@ def index():
                 <li><a href="/lab4/">Четвёртая лабораторная</a></li>
                 <li><a href="/lab5/">Пятая лабораторная</a></li>
                 <li><a href="/lab6/">Шестая лабораторная</a></li>
+                <li><a href="/lab7/">Седьмая лабораторная</a></li>
         </nav>
         <footer>
             <p>ФИО: 010595140</p>
--- lab6.py
+++ lab6.py
@@ -2,37 +2,39 @@ from flask import Blueprint, render_template, request, session
 lab6 = Blueprint('lab6', __name__)
 
 
-offices = []
+offices = [] #Каждый офис — это словарь с номером, арендатором и ценой аренды
 for i in range(1,11):
-    offices.append({"number": i, "tenant": "", "price": 900+i%3})
+    offices.append({"number": i, "tenant": "", "price": 1000})
 
 
 @lab6.route('/lab6/')
 def main():
     return render_template('lab6/lab6.html')
 
-@lab6.route('/lab6/json-rpc-api/', methods=['POST'])
+
+#этот маршрут обрабатывает запросы, которые приходят к серверу в формате JSON (это и есть API)
+@lab6.route('/lab6/json-rpc-api/', methods=['POST']) #Метод POST используется для получения данных от клиента
 def api():
-    data = request.json
-    id = data['id']
-    if data['method'] == 'info':
+    data = request.json # Получаем данные из запроса в формате JSON
+    id = data['id'] # Извлекаем ID запроса, который нужен для ответа
+    if data['method'] == 'info': # Если в методе запроса указано 'info'
         return {
             'jsonrpc': '2.0',
-            'result': offices,
-            'id': id
+            'result': offices, # Отправляем список офисов
+            'id': id # Возвращаем ID запроса
         }
 
     login = session.get('login')
-    if not login:
+    if not login: # Если пользователя нет в сессии, возвращаем ошибку авторизации
         return {
             'jsonrpc': '2.0',
             'error':{
-                'code': 1,
+                'code': 1, # Код ошибки "Неавторизован"
                 'message': 'Unauthorized'
             }
         }
 
-    if data['method'] == 'price':
+    if data['method'] == 'price': #получение суммы стоимости аренды всех офисов для текущего пользователя
         if not login:
             return{
                 'jsonrpc': '2.0',
@@ -42,21 +44,22 @@ def api():
             },
                 'id': id
             }
-        endprice = 0
-        for office in offices:
+        priceofend = 0 # Инициализируем переменную для подсчета общей стоимости аренды
+        for office in offices: # Проходим по списку офисов и суммируем стоимость аренды для офисов, арендованных текущим пользователем
             if office['tenant'] == login:
-                endprice = endprice+office['price']
+                priceofend = priceofend+office['price']
 
         return{
             'jsonrpc': '2.0',
-            'endprice': endprice,
+            'priceofend': priceofend, # Отправляем общую стоимость аренды
             'id': id
         }
     
-    if data['method'] == 'booking':
-        office_number = data['params']
+    if data['method'] == 'booking': # Обработка метода 'booking' — бронирование офиса
+        office_number = data['params'] # Получаем номер офиса из параметров запроса
         for office in offices:
             if office['number'] == office_number:
+                # Если офис уже забронирован, отправляем ошибку
                 if office['tenant'] != '':
                     return{
                         'jsonrpc': '2.0',
@@ -66,7 +69,7 @@ def api():
                         },
                         'id': id
                     }
-                office['tenant'] = login
+                office['tenant'] = login # Если офис свободен, бронируем его за текущим пользователем
                 return {
                     'jsonrpc': '2.0',
                     'result': 'succes',
@@ -74,8 +77,9 @@ def api():
                 }
     if data['method'] == 'cancellation':
         office_number = data['params']
-        for office in offices:
+        for office in offices:  # Проходим по списку офисов и ищем нужный офис по номеру
             if office['number'] == office_number:
+                # Если офис не был забронирован, возвращаем ошибку
                 if office['tenant'] == '':
                     return {
                         'jsonroc': '2.0',
@@ -85,7 +89,7 @@ def api():
                         },
                         'id': id
                     }
-                if not login:
+                if not login: # Если логин не найден в сессии, отправляем ошибку авторизации
                     return{
                         'jsonrpc': '2.0',
                         'error':{
@@ -93,7 +97,7 @@ def api():
                         'message': 'Unauthorized'
                     }
                     }
-                if office['tenant'] != login:
+                if office['tenant'] != login: # Если текущий пользователь не является арендатором этого офиса, возвращаем ошибку
                     return{
                         'jsonroc': '2.0',
                         'error':{
@@ -102,14 +106,14 @@ def api():
                         },
                         'id': id
                     }
-                office['tenant'] = ''
+                office['tenant'] = '' # Если все проверки прошли, освобождаем офис
                 return {
                     'jsonrpc': '2.0',
                     'result': 'succes',
                     'id': id
                 }
             
-    return {
+    return { # Если метод не найден, возвращаем ошибку "Метод не найден"
         'jsonrpc': '2.0',
         'error':{
             'code': -32601,
